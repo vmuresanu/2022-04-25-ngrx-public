0f95fce4af58279af6746840b9f3ae56a82b52e3
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-04-12 14:27:54 +0200

    fix config for bookings

--- libs/bookings/jest.config.js
+++ libs/bookings/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'booking',
+  displayName: 'bookings',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/libs/booking',
+  coverageDirectory: '../../coverage/libs/bookings',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },

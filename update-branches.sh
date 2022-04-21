ccfec1617a385e52f39ed1f910bbcfa21ef21b6e
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-04-21 17:47:39 +0200

    add script for updating the branches

---
+++ update-branches.sh
@@ -0,0 +1,25 @@
+set -e
+
+git checkout master
+
+branches=(master 01-architecture 02-best-practices 03-testing 04-friends)
+previous=
+current=
+
+for branch in ${branches[*]}; do
+  previous=$current
+  current=$next
+  next=$branch
+
+  if [ ! $current = "" ]
+  then
+    git checkout $current
+    git tag -d `git tag | grep -E '.'`
+    git merge $previous -m merge
+  fi;
+done
+
+git checkout $branch
+git merge $current $branch -m merge
+
+git checkout master
